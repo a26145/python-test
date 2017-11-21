aab3c4e98e6bd7de18fa106087ef7602b19d1e07
破茧-宇 <261451446@qq.com>
2017-11-21 08:51:09 +0800

    第一次上传

---
+++ test01/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: Administrator
+# @Date:   2017-11-18 23:14:52
+# @Last Modified by:   Administrator
+# @Last Modified time: 2017-11-18 23:29:57
+
+def test1():
+    print("测试")
+
+
+test1()
